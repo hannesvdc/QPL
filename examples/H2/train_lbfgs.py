b1f8ce39cfa9c1ccbdc0f4813fa166f9222896ea
Hannes Vandecasteele <hannesvdc@gmail.com>
2026-05-28 17:04:47 -0400

    line search

--- examples/H2/train_lbfgs.py
+++ examples/H2/train_lbfgs.py
@@ -55,7 +55,7 @@ chunk_size = 4
 loss_fcn = EnergyLoss( chunk_size=chunk_size )
 
 # Setup the optimizer and learning rate scheduler
-optimizer = optim.LBFGS( model.parameters() )
+optimizer = optim.LBFGS( model.parameters(), line_search_fn='stronge_wolfe' )
 
 # Main training routine
 train_counter : List = []
